4c1a2c7fe03c690777d1f9c7622e121ab94dbb61
Carsten <18394207+Aschratt@users.noreply.github.com>
2020-03-30 22:57:45 +0200

    Include from public headers.

--- src/Core/include/litefx/litefx.h
+++ src/Core/include/litefx/litefx.h
@@ -1,14 +1,15 @@
 #pragma once
 
-#include "core.h"
-#include "rendering.hpp"
+#include <litefx/core.h>
+#include <litefx/core_types.hpp>
+#include <litefx/rendering.hpp>
 
 #if defined(BUILD_OPEN_GL_BACKEND)
-#  include "backends/opengl.hpp"
+#  include <litefx/backends/opengl.hpp>
 #endif // defined(BUILD_OPEN_GL_BACKEND)
 
 #if defined(BUILD_VULKAN_BACKEND)
-#  include "backends/vulkan.hpp"
+#  include <litefx/backends/vulkan.hpp>
 #endif // defined(BUILD_VULKAN_BACKEND)
 
 #if defined(LITEFX_AUTO_IMPORT_NAMESPACE)
