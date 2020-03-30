a57664f0a3310864afed4a46d9531ad71e8b4319
Carsten <18394207+Aschratt@users.noreply.github.com>
2020-03-30 22:10:57 +0200

    Made litefx.h static again.

--- src/Core/include/litefx/litefx.h
+++ src/Core/include/litefx/litefx.h
@@ -11,3 +11,6 @@
 #  include "backends/vulkan.hpp"
 #endif // defined(BUILD_VULKAN_BACKEND)
 
+#if defined(LITEFX_AUTO_IMPORT_NAMESPACE)
+using namespace LiteFX;
+#endif // defined(LITEFX_AUTO_IMPORT_NAMESPACE)
