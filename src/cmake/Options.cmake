0ebfae30583084740043dec61bde9227a23ea0c9
Carsten <18394207+Aschratt@users.noreply.github.com>
2020-03-31 10:30:04 +0200

    Added engine identifier build option.

--- src/cmake/Options.cmake
+++ src/cmake/Options.cmake
@@ -10,3 +10,5 @@ OPTION(BUILD_VULKAN_BACKEND "Builds the Vulkan backend." ON)
 #OPTION(BUILD_DIRECTX_12_BACKEND "Builds the DirectX 12 backend." OFF)
 
 OPTION(BUILD_SAMPLE_BASIC_RENDERING "Builds the basic rendering sample." ON)
+
+SET(BUILD_ENGINE_IDENTIFIER "LiteFX" CACHE STRING "Defines the engine identifier string.")
