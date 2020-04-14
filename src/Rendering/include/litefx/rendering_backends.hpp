2c4ff261ae9aee680669ccb270c39528afeee8d4
Carsten <18394207+Aschratt@users.noreply.github.com>
2020-04-14 13:02:39 +0200

    Restored graphics device builder.

--- src/Rendering/include/litefx/rendering_backends.hpp
+++ src/Rendering/include/litefx/rendering_backends.hpp
@@ -6,6 +6,27 @@
 namespace LiteFX::Rendering {
     using namespace LiteFX;
 
+    class LITEFX_RENDERING_API GraphicsDeviceBuilder : public Builder<IGraphicsDevice> {
+        LITEFX_IMPLEMENTATION(GraphicsDeviceBuilderImpl)
+
+    public:
+        explicit GraphicsDeviceBuilder(const IRenderBackend* backend);
+        GraphicsDeviceBuilder(const GraphicsDeviceBuilder&&) = delete;
+        GraphicsDeviceBuilder(GraphicsDeviceBuilder&) = delete;
+        virtual ~GraphicsDeviceBuilder() noexcept;
+
+    public:
+        virtual const IRenderBackend* getBackend() const noexcept;
+        virtual const ISurface* getSurface() const noexcept;
+        virtual const IGraphicsAdapter* getAdapter() const noexcept;
+
+    public:
+        virtual GraphicsDeviceBuilder& forSurface(const ISurface* surface);
+        virtual GraphicsDeviceBuilder& useAdapter(const uint32_t& adapter);
+        virtual GraphicsDeviceBuilder& useAdapterOrDefault(const Optional<uint32_t>& adapterId = std::nullopt);
+    };
+
+
     class LITEFX_RENDERING_API IRenderBackend : public IBackend {
     public:
         virtual ~IRenderBackend() noexcept = default;
@@ -13,6 +34,9 @@ namespace LiteFX::Rendering {
     public:
         virtual Array<const IGraphicsAdapter*> getAdapters() const = 0;
         virtual const IGraphicsAdapter* getAdapter(const Optional<uint32_t>& adapterId = std::nullopt) const = 0;
+
+    public:
+        virtual GraphicsDeviceBuilder& buildDevice() const = 0;
     };
 
     class LITEFX_RENDERING_API RenderBackend : public IRenderBackend {
@@ -28,5 +52,4 @@ namespace LiteFX::Rendering {
         virtual BackendType getType() const noexcept override;
         const App& getApp() const noexcept;
     };
-
 }
