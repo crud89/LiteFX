bcbbaa7e18570fed009f4471fcb02191df81fd71
Carsten <18394207+Aschratt@users.noreply.github.com>
2020-07-22 17:01:12 +0200

    Properly initialize image base class.

--- src/Rendering/src/texture.cpp
+++ src/Rendering/src/texture.cpp
@@ -13,13 +13,11 @@ public:
 private:
     const IDescriptorLayout* m_layout;
     MultiSamplingLevel m_samples;
-    UInt32 m_elements, m_size, m_levels;
-    Format m_format;
-    Size2d m_extent;
+    UInt32 m_levels;
 
 public:
-    TextureImpl(Texture* parent, const IDescriptorLayout* layout, const UInt32& elements, const UInt32& size, const Size2d& extent, const Format& format, const UInt32& levels, const MultiSamplingLevel& samples) :
-        base(parent), m_layout(layout), m_elements(elements), m_size(size), m_extent(extent), m_format(format), m_samples(samples), m_levels(levels) { }
+    TextureImpl(Texture* parent, const IDescriptorLayout* layout, const UInt32& levels, const MultiSamplingLevel& samples) :
+        base(parent), m_layout(layout), m_samples(samples), m_levels(levels) { }
 };
 
 // ------------------------------------------------------------------------------------------------
@@ -27,7 +25,7 @@ public:
 // ------------------------------------------------------------------------------------------------
 
 Texture::Texture(const IDescriptorLayout* layout, const UInt32& elements, const UInt32& size, const Size2d& extent, const Format& format, const UInt32& levels, const MultiSamplingLevel& samples) :
-    m_impl(makePimpl<TextureImpl>(this, layout, elements, size, extent, format, levels, samples))
+    m_impl(makePimpl<TextureImpl>(this, layout, levels, samples)), Image(elements, size, extent, format)
 {
     if (layout == nullptr)
         throw std::invalid_argument("The constant buffer descriptor layout must be initialized.");
