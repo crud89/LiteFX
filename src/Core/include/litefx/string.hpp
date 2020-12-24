6614963b9821d1cac67d23b4df360b9d4754a7de
Carsten <18394207+Aschratt@users.noreply.github.com>
2020-12-24 11:19:55 +0100

    Added UTF-8/UTF-16 conversion helpers.

--- src/Core/include/litefx/string.hpp
+++ src/Core/include/litefx/string.hpp
@@ -4,9 +4,11 @@
 #include <sstream>
 #include <iterator>
 #include <vector>
+#include <codecvt>
 
 namespace LiteFX {
     using String = std::string;
+    using WString = std::wstring;
 
     // Based on: https://stackoverflow.com/a/5289170/1254352
     template <typename TStrings, typename Value = typename TStrings::value_type>
@@ -29,4 +31,25 @@ namespace LiteFX {
         return stream.str();
     }
 
+    /// <summary>
+    /// Converts an UTF-8 single-byte encoded string into an UTF-16 representation.
+    /// </summary>
+    /// <param name="utf8"></param>
+    /// <returns></returns>
+    inline WString Widen(const String& utf8)
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<WCHAR>> conv;
+        return conv.from_bytes(utf8);
+    }
+
+    /// <summary>
+    /// Converts an UTF-16 multi-byte encoded string into an UTF-8 representation.
+    /// </summary>
+    /// <param name="utf16"></param>
+    /// <returns></returns>
+    inline String Narrow(const WString& utf16)
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<WCHAR>> conv;
+        return conv.to_bytes(utf16);
+    }
 }
