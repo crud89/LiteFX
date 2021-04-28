781cef284b422b4b8bde46633e38f2c936d6cf42
Carsten <18394207+Aschratt@users.noreply.github.com>
2021-04-28 14:24:54 +0200

    Use unsigned integers for flag tests.

--- src/Core/include/litefx/containers.hpp
+++ src/Core/include/litefx/containers.hpp
@@ -28,7 +28,7 @@
 #endif
 
 #ifndef LITEFX_FLAG_IS_SET
-#  define LITEFX_FLAG_IS_SET(val, flag) static_cast<int>(val & flag) == static_cast<int>(flag)
+#  define LITEFX_FLAG_IS_SET(val, flag) static_cast<UInt32>(val & flag) == static_cast<UInt32>(flag)
 #endif
 
 namespace LiteFX {
