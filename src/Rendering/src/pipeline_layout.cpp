e5bdf3f64d66c0a2ad6334825dbe43b4a57d11c5
Carsten <18394207+Aschratt@users.noreply.github.com>
2021-05-04 12:38:11 +0200

    Remove states from pipeline layout.

--- src/Rendering/src/pipeline_layout.cpp
+++ src/Rendering/src/pipeline_layout.cpp
@@ -11,41 +11,15 @@ public:
 	friend class RenderPipelineLayout;
 
 private:
-	Array<UniquePtr<IViewport>> m_viewports;
 	UniquePtr<IRasterizer> m_rasterizer;
 	UniquePtr<IInputAssembler> m_inputAssembler;
 	UniquePtr<IShaderProgram> m_shaderProgram;
 	bool m_depthTest = false, m_stencilTest = false;
 
 public:
-	RenderPipelineLayoutImpl(RenderPipelineLayout* parent) : base(parent) { }
-
-public:
-	Array<IViewport*> getViewports() const noexcept 
+	RenderPipelineLayoutImpl(RenderPipelineLayout* parent) :
+		base(parent) 
 	{
-		Array<IViewport*> viewports(m_viewports.size());
-		std::generate(std::begin(viewports), std::end(viewports), [&, i = 0]() mutable { return m_viewports[i++].get(); });
-		return viewports;
-	}
-	
-	void add(UniquePtr<IViewport>&& viewport) 
-	{
-		m_viewports.push_back(std::move(viewport));
-	}
-	
-	UniquePtr<IViewport> remove(const IViewport* viewport) noexcept
-	{
-		auto it = std::find_if(m_viewports.begin(), m_viewports.end(), [viewport](const UniquePtr<IViewport>& a) { return a.get() == viewport; });
-
-		if (it == m_viewports.end())
-			return UniquePtr<IViewport>();
-		else
-		{
-			auto result = std::move(*it);
-			m_viewports.erase(it);
-
-			return std::move(result);
-		}
 	}
 };
 
@@ -59,21 +33,6 @@ RenderPipelineLayout::RenderPipelineLayout() :
 }
 
 RenderPipelineLayout::~RenderPipelineLayout() noexcept = default;
-
-Array<IViewport*> RenderPipelineLayout::getViewports() const noexcept 
-{
-	return m_impl->getViewports();
-}
-
-void RenderPipelineLayout::use(UniquePtr<IViewport>&& viewport) 
-{
-	return m_impl->add(std::move(viewport));
-}
-
-UniquePtr<IViewport> RenderPipelineLayout::remove(const IViewport* viewport) noexcept
-{
-	return m_impl->remove(viewport);
-}
 
 IRasterizer* RenderPipelineLayout::getRasterizer() const noexcept 
 {
