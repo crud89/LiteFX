0fcf83ab89b062f84969aa1689f492e34477fff5
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-06-01 18:14:50 +0200

    Removed obsolete window pointer retrieval.

--- src/Samples/Vulkan/Textures/src/main.cpp
+++ src/Samples/Vulkan/Textures/src/main.cpp
@@ -39,9 +39,7 @@ int main(const int argc, const char** argv)
 
 	::glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	::glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
 	auto window = GlfwWindowPtr(::glfwCreateWindow(800, 600, appName.c_str(), nullptr, nullptr));
-	const auto windowPtr = window.get();
 
 	// Get the required extensions from glfw.
 	uint32_t extensions = 0;
