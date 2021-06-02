5fdff14c39d2163b5634b83e98fd15c4cfb71ce5
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-06-02 09:30:26 +0200

    Create RTVs for output attachments.

--- src/Backends/DirectX12/src/frame_buffer.cpp
+++ src/Backends/DirectX12/src/frame_buffer.cpp
@@ -14,6 +14,8 @@ private:
     Array<UniquePtr<IDirectX12Image>> m_outputAttachments;
     Array<const IDirectX12Image*> m_renderTargetViews;
     UniquePtr<DirectX12CommandBuffer> m_commandBuffer;
+    ComPtr<ID3D12DescriptorHeap> m_renderTargetHeap;
+    UInt32 m_renderTargetDescriptorSize;
     Size2d m_size;
     UInt32 m_bufferIndex;
 
@@ -30,6 +32,43 @@ public:
     {
         // TODO: Clear existing RTVs.
         // TODO: Create RTVs and receive RTVs from swap chain for present targets.
+
+
+        // Create descriptor heap for RTVs.
+        D3D12_DESCRIPTOR_HEAP_DESC renderTargetHeapDesc = {};
+        renderTargetHeapDesc.NumDescriptors = static_cast<UInt32>(m_parent->parent().renderTargets().size());
+        renderTargetHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+
+        raiseIfFailed<RuntimeException>(m_parent->getDevice()->handle()->CreateDescriptorHeap(&renderTargetHeapDesc, IID_PPV_ARGS(&m_renderTargetHeap)), "Unable to create render target descriptor heap.");
+        m_renderTargetDescriptorSize = m_parent->getDevice()->handle()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+        CD3DX12_CPU_DESCRIPTOR_HANDLE renderTargetViewDescriptor(m_renderTargetHeap->GetCPUDescriptorHandleForHeapStart());
+
+        // Initialize the output attachments from render targets of the parent render pass.
+        // NOTE: Again, we assume, that the parent render pass provides the render targets in an sorted manner.
+        std::ranges::for_each(m_parent->parent().renderTargets(), [&, i = 0](const RenderTarget& renderTarget) mutable {
+            if (renderTarget.location() != i++) [[unlikely]]
+                LITEFX_WARNING(DIRECTX12_LOG, "Remapped render target from location {0} to location {1}. Please make sure that the render targets are sorted within the render pass and do not have any gaps in their location mappings.", renderTarget.location(), i - 1);
+            
+            const IDirectX12Image* renderTargetView;
+
+            if (renderTarget.type() == RenderTargetType::Present)
+            {
+                // If the render target is a present target, acquire an image view from the swap chain.
+                auto swapChainImages = m_parent->getDevice()->swapChain().images();
+                renderTargetView = swapChainImages[m_bufferIndex];
+            }
+            else
+            {
+                // Create an image view for the render target.
+                auto image = m_parent->getDevice()->factory().createAttachment(renderTarget.format(), m_size, renderTarget.samples());
+                renderTargetView = image.get();
+                m_outputAttachments.push_back(std::move(image));
+            }
+
+            m_parent->getDevice()->handle()->CreateRenderTargetView(renderTargetView->handle().Get(), nullptr, renderTargetViewDescriptor);
+            m_renderTargetViews.push_back(renderTargetView);
+        });
+
         throw;
     }
 };
