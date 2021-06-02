1ef5687f282c8777e73c12079195653a7c8da03e
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-06-02 14:47:33 +0200

    Move allocator and allocation pointer declarations.

--- src/Backends/DirectX12/src/buffer.h
+++ src/Backends/DirectX12/src/buffer.h
@@ -2,5 +2,17 @@
 
 #include <litefx/rendering.hpp>
 #include <litefx/backends/dx12.hpp>
-
 #include "D3D12MemAlloc.h"
+
+namespace LiteFX::Rendering::Backends {
+	using namespace LiteFX::Rendering;
+
+	struct D3D12MADeleter {
+		void operator()(auto* ptr) noexcept {
+			ptr->Release();
+		}
+	};
+
+	typedef SharedPtr<D3D12MA::Allocator> AllocatorPtr;
+	typedef UniquePtr<D3D12MA::Allocation, D3D12MADeleter> AllocationPtr;
+}
