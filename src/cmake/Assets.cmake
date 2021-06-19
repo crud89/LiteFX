bf3e7eabaad391e2d26c445c40adf7371c56e3aa
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-06-19 17:46:56 +0200

    Define assets by target directory.

--- src/cmake/Assets.cmake
+++ src/cmake/Assets.cmake
@@ -4,43 +4,65 @@
 #####                                                                                         #####
 ###################################################################################################
 
-# The script exports one functions:
+# The script exports one function:
 #
-# - TARGET_ADD_ASSETS: Defines a dependency to a list of static asset files, that are copied to the runtime directory and creates an install command for each.
+# - TARGET_ADD_ASSET_DIRECTORY: Defines a directory within the runtime output and a set of static assets that are copied to it. Also defines 
+#                               an install command for each asset.
+#
+# The following sample demonstrates how to use this command:
+#
+# TARGET_ADD_ASSET_DIRECTORY(my_project
+#   NAME "assets/textures"
+#   INSTALL_DESTINATION "bin/"
+#   ASSETS "images/a.tga" "textures/b.tga"
+# )
+#
+# In the example above, one asset directory is defined for the target 'my_project'. The directory will be created in the runtime output 
+# directory, if CMAKE_RUNTIME_OUTPUT_DIRECTORY is not empty, or in CMAKE_CURRENT_BINARY_DIR directory, if it is empty. The directory and 
+# possible subdirectories are defined by the NAME parameter.
+#
+# The INSTALL_DESTINATION specifies the destination, the directory will be installed to by the created install command. Note that the parameter
+# is always prepended by CMAKE_INSTALL_PREFIX.
+#
+# The ASSETS parameter defines a list of the files that are copied to the directory during the build. Note that you have to ensure, that no two 
+# file names are the same, since it would result in one of the files being overwritten.
 
-SET(ASSET_DEFAULT_SUBDIR "assets" CACHE STRING "Default subdirectory for static assets within the current binary directory (CMAKE_CURRENT_BINARY_DIR).")
+FUNCTION(TARGET_ADD_ASSET_DIRECTORY target_name)
+  CMAKE_PARSE_ARGUMENTS(ASSET_DIRECTORY "" "NAME;INSTALL_DESTINATION" "ASSETS" ${ARGN})
+  STRING(REPLACE "/" "-" directory_target_name ${ASSET_DIRECTORY_NAME})
+  SET(directory_target_name "${target_name}-${directory_target_name}")
 
-FUNCTION(TARGET_ADD_ASSETS target_name)
-  CMAKE_PARSE_ARGUMENTS(ASSET "" "INSTALL_DESTINATION" "ASSETS" ${ARGN})
+  GET_TARGET_PROPERTY(folder_name ${target_name} FOLDER)
   
   IF(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} STREQUAL "")
-    SET(OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/${ASSET_DEFAULT_SUBDIR})
+    SET(OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/${ASSET_DIRECTORY_NAME})
   ELSE()
-    SET(OUTPUT_DIR ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${ASSET_DEFAULT_SUBDIR})
+    SET(OUTPUT_DIR ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${ASSET_DIRECTORY_NAME})
   ENDIF(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} STREQUAL "")
 
-  ADD_CUSTOM_TARGET(${target_name}-assets
+  ADD_CUSTOM_TARGET(${directory_target_name}
     COMMENT "Copying assets to runtime directory '${OUTPUT_DIR}'..."
   )
 
-  ADD_CUSTOM_COMMAND(TARGET ${target_name}-assets 
+  ADD_CUSTOM_COMMAND(TARGET ${directory_target_name}
     COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPUT_DIR}
   )
     
-  SET_TARGET_PROPERTIES(${target_name}-assets PROPERTIES 
-    SOURCES ${ASSET_ASSETS}
+  SET_TARGET_PROPERTIES(${directory_target_name} PROPERTIES 
+    SOURCES ${ASSET_DIRECTORY_ASSETS}
     RUNTIME_OUTPUT_DIRECTORY ${OUTPUT_DIR}
+    FOLDER ${folder_name}
   )
 
-  FOREACH(ASSET_FILE ${ASSET_ASSETS})
+  FOREACH(ASSET_FILE ${ASSET_DIRECTORY_ASSETS})
     GET_FILENAME_COMPONENT(ASSET_NAME ${ASSET_FILE} NAME)
-    ADD_CUSTOM_COMMAND(TARGET ${target_name}-assets 
+    ADD_CUSTOM_COMMAND(TARGET ${directory_target_name} 
       COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/${ASSET_FILE} ${OUTPUT_DIR}/${ASSET_NAME}
       DEPENDS ${ASSET_FILE}
     )
+  ENDFOREACH(ASSET_FILE ${ASSET_DIRECTORY_ASSETS})
 
-    INSTALL(FILES ${CMAKE_CURRENT_SOURCE_DIR}/${ASSET_FILE} DESTINATION ${CMAKE_INSTALL_PREFIX}/${ASSET_INSTALL_DESTINATION})
-  ENDFOREACH(ASSET_FILE ${ASSET_ASSETS})
+  INSTALL(DIRECTORY ${OUTPUT_DIR} DESTINATION ${CMAKE_INSTALL_PREFIX}/${ASSET_DIRECTORY_INSTALL_DESTINATION})
 
-  ADD_DEPENDENCIES(${target_name} ${target_name}-assets)
-ENDFUNCTION(TARGET_ADD_ASSETS target_name)
+  ADD_DEPENDENCIES(${target_name} ${directory_target_name})
+ENDFUNCTION(TARGET_ADD_ASSET_DIRECTORY target_name)
