e12ec95912f27ff6e59cf8f8022728a2d362cc34
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-06-19 20:38:48 +0200

    Changed version format.

--- src/cmake/Version.cmake
+++ src/cmake/Version.cmake
@@ -4,6 +4,10 @@
 #####                                                                                         #####
 ###################################################################################################
 
+# The year part of the version is actually ignored when it comes to compatibility. Different RELEASE versions are generally considered 
+# incompatible, whilst PATCH versions are considered generally backwards compatible. Different BUILD versions don't affect compatibility,
+# but instead only contain bug fixes.
+
 SET(LITEFX_YEAR 2021)
 SET(LITEFX_RELEASE 1)
 SET(LITEFX_PATCH 0)
