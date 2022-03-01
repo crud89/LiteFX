af52b775b8f0d72551f15c11ffed30a2d3eb263d
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2022-03-01 14:13:44 +0100

    Fix writing manifest file.

--- write-version.ps1
+++ write-version.ps1
@@ -32,14 +32,14 @@ $output.Substring(0, $output.Length - 1) | Out-File -Encoding "UTF8" -FilePath .
 $output = ""
 
 Get-Content .\src\vcpkg.json | ForEach-Object {
-    if ($_.Trim().StartsWith("version-string")) {
-        $output += "  `"version-string`": `"{0}.{1}.{2}.{3}`"`r`n" -f $major, $minor, $build, $year
+    if ($_.Trim().StartsWith("`"version-string`"")) {
+        $output += "  `"version-string`": `"{0}.{1}.{2}.{3}`",`r`n" -f $major, $minor, $build, $year
     } else {
         $output += "{0}`r`n" -f $_
     }
 }
 
-$output.Substring(0, $output.Length - 1) | Out-File -Encoding "ASCII" -FilePath .\src\vcpkg.json
+$output.Substring(0, $output.Length - 2) | Out-File -Encoding "ASCII" -FilePath .\src\vcpkg.json
 
 # Write new version to console.
 $output = "Updated Version to {0}.{1}.{2}.{3}" -f $major, $minor, $build, $year
