64c8c7337c8c6b1a0f6ceb345957da4f06b8a49f
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2023-04-06 17:11:14 +0200

    Disable (potential) false positive barrier error.
    This only happens when ending a render pass that draws directly to a swap-chain back buffer. I reported this to MSFT using discord and they are investigating. Since all barriers of the app are now new enhanced barriers, we should be safe to ignore this error in the long run.

--- src/Backends/DirectX12/src/device.cpp
+++ src/Backends/DirectX12/src/device.cpp
@@ -127,7 +127,11 @@ public:
 			//infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_INFO, TRUE);
 			
 			// Suppress individual messages by their ID
-			D3D12_MESSAGE_ID suppressIds[] = { D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE, D3D12_MESSAGE_ID_CLEARDEPTHSTENCILVIEW_MISMATCHINGCLEARVALUE };
+			D3D12_MESSAGE_ID suppressIds[] = { 
+				D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE, // Mismatch in clear value is intended.
+				D3D12_MESSAGE_ID_CLEARDEPTHSTENCILVIEW_MISMATCHINGCLEARVALUE, // Mismatch in clear value is intended.
+				D3D12_MESSAGE_ID_BARRIER_INTEROP_INVALID_STATE                // Temporary workaround, as this appears to be a false-positive here... Reported to MSFT and they've got an internal bug (44057285) tracked for this. See: https://github.com/crud89/d3d12-renderpass-barrier-mwe.
+			};
 			D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };	// Somehow it is required to deny info-level messages. Otherwise strange pointer issues are occurring.
 
 			D3D12_INFO_QUEUE_FILTER infoQueueFilter = {};
