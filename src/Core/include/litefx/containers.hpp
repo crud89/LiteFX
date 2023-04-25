5e0a87a42349b1074d81bfa77f758aef5334ed9b
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2023-04-25 22:04:29 +0200

    Update enumerable class to forward input elements.

--- src/Core/include/litefx/containers.hpp
+++ src/Core/include/litefx/containers.hpp
@@ -11,6 +11,7 @@
 #include <optional>
 #include <map>
 #include <vector>
+#include <forward_list>
 #include <queue>
 #include <tuple>
 #include <memory>
@@ -181,82 +182,187 @@ namespace LiteFX {
 	}
 
 	/// <summary>
-	/// Provides foward-iteratable access to a range of elements of type <typeparamref name="T" /> from arbitrary range inputs and
+	/// Describes an intermediate container for elements of type <typeparamref name="T" />.
 	/// </summary>
-	/// <typeparam name="T">The type of the elements of the range.</typeparam>
+	/// <remarks>
+	/// An `Enumerable` is a generic runtime-polymorphic container designed for class interfaces. Differently to STL containers, it does not impose constraints other than the 
+	/// requirement of being forward-iterable. It's purpose is to pass immutable containers between objects. Since it is compatible to the STL *ranges* library, it can be 
+	/// constructed from arbitrary containers.
+	/// 
+	/// Note that `Enumerable` *owns* it's elements, which means that a copy *might* occur, if the input range does not contain rvalue references. You might want to wrap the
+	/// element type in a <see cref="Ref" />, if you want to provide access to source elements. If you want to transfer ownership of the elements to the `Enumerable`, you can 
+	/// use `std::views::as_rvalue` on the source range or view. This is required for non-copyable (move-only) types, such as <see cref="UniquePtr" />. Note, however, that this 
+	/// might leave the original container in an undefined state.
+	/// </remarks>
+	/// <typeparam name="T">The type of the container elements.</typeparam>
 	template <typename T>
 	class Enumerable {
 	public:
 		using value_type = T;
-		using array_type = Array<T>;
+		using array_type = std::forward_list<T>;
+		using allocator_type = array_type::allocator_type;
+		using size_type = array_type::size_type;
+		using difference_type = array_type::difference_type;
+		using pointer = array_type::pointer;
+		using const_pointer = array_type::const_pointer;
 		using iterator = array_type::iterator;
 		using const_iterator = array_type::const_iterator;
-		using size_type = array_type::size_type;
+		using reference = value_type&;
+		using const_reference = const value_type&;
 
 	private:
-		Array<T> m_elements;
+		array_type m_elements;
+		size_type m_size;
 
 	public:
 		/// <summary>
-		/// Initializes a new enumerable range from an arbitrary input range.
+		/// Creates a new `Enumerable` from an arbitrary input range or view.
 		/// </summary>
-		/// <param name="input">The input range containing the elements of the range.</param>
+		/// <param name="input">The input range or view that contains the elements the `Enumerable` is initialized with.</param>
 		constexpr Enumerable(std::ranges::input_range auto&& input) noexcept requires
 			std::convertible_to<std::ranges::range_value_t<decltype(input)>, T>
 		{
-			if constexpr (std::ranges::sized_range<decltype(input)>)
-				m_elements.reserve(std::ranges::size(input));
+			auto it = m_elements.before_begin();
+			m_size = 0;
 
-			std::ranges::move(input, std::back_inserter(m_elements));
+			for (auto elem : input)
+			{
+				m_elements.insert_after(it, std::forward<std::ranges::range_value_t<decltype(input)>>(elem));
+				it++;
+				m_size++;
+			}
 		}
 
 		/// <summary>
-		/// Initializes a new enumerable range from an initializer list.
+		/// Creates a new `Enumerable` from an initializer list.
 		/// </summary>
-		/// <param name="input">The initializer list containing the elements of the range.</param>
+		/// <param name="input">The initializer list that contains the elements, the `Enumerable` is initialized with.</param>
 		constexpr explicit Enumerable(std::initializer_list<T> input) noexcept :
-			m_elements{ input } { }
+			m_elements{ input }, m_size(input.size())
+		{
+		}
 
 		/// <summary>
-		/// Initializes a new empty enumerable range.
+		/// Creates a new `Enumerable` from a forward list.
 		/// </summary>
-		constexpr Enumerable() noexcept = default;
+		/// <remarks>
+		/// Initializing the enumerable by moving a forward list may be more efficient than the other constructors.
+		/// </remarks>
+		/// <param name="input">The forward list that contains the elements, the `Enumerable` is initialized with.</param>
+		constexpr Enumerable(array_type&& input) noexcept :
+			m_elements(std::move(input)), m_size(0)
+		{
+			for (auto& elem : m_elements)
+				m_size++;
+		}
+
+		/// <summary>
+		/// Creates an empty `Enumerable`.
+		/// </summary>
+		constexpr Enumerable() requires std::default_initializable<array_type> = default;
+
+		/// <summary>
+		/// Initializes the `Enumerable` by taking over <paramref name="_other" />.
+		/// </summary>
+		/// <remarks>
+		/// Note that this constructor can only be used of <typeparamref name="T" /> is movable.
+		/// </remarks>
+		/// <param name="_other">The `Enumerable` to take over.</param>
+		constexpr Enumerable(Enumerable<T>&& _other) = default;
+
+		/// <summary>
+		/// Initializes the `Enumerable` by taking over <paramref name="_other" />.
+		/// </summary>
+		/// <remarks>
+		/// Note that this constructor can only be used of <typeparamref name="T" /> is movable.
+		/// </remarks>
+		/// <param name="_other">The `Enumerable` to take over.</param>
+		/// <returns>A reference of the `Enumerable` after the move.</returns>
+		constexpr Enumerable<T>& operator=(Enumerable<T>&& _other) = default;
+
+		/// <summary>
+		/// Initializes the `Enumerable` by copying <paramref name="_other" />.
+		/// </summary>
+		/// <remarks>
+		/// Note that this constructor can only be used of <typeparamref name="T" /> is copyable.
+		/// </remarks>
+		/// <param name="_other">The `Enumerable` to copy.</param>
+		constexpr Enumerable(const Enumerable<T>& _other) = default;
+
+		/// <summary>
+		/// Initializes the `Enumerable` by copying <paramref name="_other" />.
+		/// </summary>
+		/// <remarks>
+		/// Note that this constructor can only be used of <typeparamref name="T" /> is copyable.
+		/// </remarks>
+		/// <param name="_other">The `Enumerable` to copy.</param>
+		/// <returns>A reference of the `Enumerable` after the copy.</returns>
+		constexpr Enumerable<T>& operator=(const Enumerable<T>& _other) = default;
 
 	public:
-		Enumerable(const Enumerable&) = default;
-		Enumerable(Enumerable&&) = default;
-		virtual ~Enumerable() noexcept = default;
-
-	public:
 		/// <summary>
-		/// Returns an iterator providing access to the front of the range.
+		/// Returns the number of elements of the `Enumerable`.
 		/// </summary>
-		/// <returns>The iterator providing access to the front of the range.</returns>
-		constexpr auto begin() const noexcept { return m_elements.begin(); }
+		/// <returns>The number of elements of the `Enumerable`.</returns>
+		constexpr size_type size() const noexcept {
+			return m_size;
+		}
 
 		/// <summary>
-		/// Returns an iterator that points to the end of the range.
+		/// Returns `true`, if the `Enumerable` is empty and `false` otherwise.
 		/// </summary>
-		/// <returns>The iterator points to the end of the range.</returns>
-		constexpr auto end() const noexcept { return m_elements.end(); }
+		/// <returns>`true`, if the `Enumerable` is empty and `false` otherwise.</returns>
+		constexpr bool empty() const noexcept {
+			return m_elements.empty();
+		}
 
 		/// <summary>
-		/// Returns `true`, if the range does not contain any elements and `false` otherwise.
+		/// Returns the iterator that points to the beginning of the `Enumerable`.
 		/// </summary>
-		/// <returns>`true`, if the range does not contain any elements and `false` otherwise.</returns>
-		constexpr auto empty() const noexcept { return m_elements.empty(); }
+		/// <returns>The iterator that points to the beginning of the `Enumerable`.</returns>
+		constexpr iterator begin() noexcept {
+			return m_elements.begin();
+		}
 
 		/// <summary>
-		/// Returns the number of elements in the range.
+		/// Returns the iterator that points to the beginning of the `Enumerable`.
 		/// </summary>
-		/// <returns>The number of elements in the range.</returns>
-		constexpr auto size() const noexcept { return m_elements.size(); }
+		/// <returns>The iterator that points to the beginning of the `Enumerable`.</returns>
+		constexpr const_iterator cbegin() const noexcept {
+			return m_elements.cbegin();
+		}
 
 		/// <summary>
-		/// Returns the first element of the range.
+		/// Returns the iterator that points to the ending of the `Enumerable`.
 		/// </summary>
-		/// <returns>The first element of the range.</returns>
-		constexpr auto& front() const { return m_elements.front(); }
+		/// <returns>The iterator that points to the ending of the `Enumerable`.</returns>
+		constexpr iterator end() noexcept {
+			return m_elements.end();
+		}
+
+		/// <summary>
+		/// Returns the iterator that points to the ending of the `Enumerable`.
+		/// </summary>
+		/// <returns>The iterator that points to the ending of the `Enumerable`.</returns>
+		constexpr const_iterator cend() const noexcept {
+			return m_elements.cend();
+		}
+
+		/// <summary>
+		/// Returns the first element of the `Enumerable`, if it is not empty.
+		/// </summary>
+		/// <returns>The first element of the `Enumerable`, if it is not empty.</returns>
+		constexpr decltype(auto) front() {
+			return m_elements.front();
+		}
+
+		/// <summary>
+		/// Returns the first element of the `Enumerable`, if it is not empty.
+		/// </summary>
+		/// <returns>The first element of the `Enumerable`, if it is not empty.</returns>
+		constexpr decltype(auto) front() const {
+			return m_elements.front();
+		}
 	};
 
 #if (defined(BUILD_LITEFX_PIMPL) && BUILD_LITEFX_PIMPL) || (!defined(BUILD_LITEFX_PIMPL)) && !defined(LITEFX_IMPLEMENTATION)
