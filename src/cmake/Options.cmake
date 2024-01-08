b71a69668640860dd9edd10497dc4ec5a86448eb
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2024-01-08 16:50:27 +0100

    Bump default shader model to 6.5 to support mesh shaders.

--- src/cmake/Options.cmake
+++ src/cmake/Options.cmake
@@ -23,7 +23,7 @@ OPTION(BUILD_WITH_DIRECTX_MATH "Enables DirectXMath converters for math types." 
 FIND_PROGRAM(BUILD_GLSLC_COMPILER glslc HINTS ENV VULKAN_SDK PATH_SUFFIXES bin DOC "The full path to the `glslc.exe` shader compiler binary.")
 FIND_PROGRAM(BUILD_DXC_COMPILER dxc HINTS ENV VULKAN_SDK PATH_SUFFIXES bin DOC "The full path to the `dxc.exe` shader compiler binary.")
 
-SET(BUILD_HLSL_SHADER_MODEL "6_3" CACHE STRING "Shader model version used to compile HLSL shaders.")
+SET(BUILD_HLSL_SHADER_MODEL "6_5" CACHE STRING "Shader model version used to compile HLSL shaders.")
 
 OPTION(BUILD_PRECOMPILED_HEADERS "Use pre-compiled headers during build." OFF)
 
