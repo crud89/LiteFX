e27b6b793fc0413a6d94fcedccd20ef714b7d914
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2024-01-24 12:55:29 +0100

    Add alignment helper.

--- src/Math/include/litefx/math.hpp
+++ src/Math/include/litefx/math.hpp
@@ -76,6 +76,18 @@ namespace LiteFX::Math {
 		return static_cast<Double>(arg);
 	}
 
+	/// <summary>
+	/// Aligns a value <paramref name="size" /> to an alignment <paramref name="alignment" />.
+	/// </summary>
+	/// <typeparam name="T">The type of the value to align.</typeparam>
+	/// <param name="size">The value to align.</param>
+	/// <param name="alignment">The alignment to align the value to.</param>
+	/// <returns>The aligned value.</returns>
+	template <typename T = UInt32>
+	static constexpr inline T align(T size, T alignment) { 
+		return (size + alignment - 1) & ~(alignment - 1); 
+	}
+
 #pragma region Vector
 	class LITEFX_MATH_API Vector1f : public Vector<Float, 1> {
 	public:
