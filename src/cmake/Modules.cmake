2f97f8a3a4bab0f603f18326c62859d75f94f8e3
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2024-06-22 14:00:34 +0200

    Allow switching to static linking.

--- src/cmake/Modules.cmake
+++ src/cmake/Modules.cmake
@@ -9,5 +9,12 @@ IF("${CMAKE_MODULE_PATH}" STREQUAL "")
     SET(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/" CACHE PATH "Module directory." FORCE)
 ENDIF("${CMAKE_MODULE_PATH}" STREQUAL "")
 
+# This must be defined before the top-level `PROJECT()` call.
+OPTION(BUILD_SHARED_LIBS "Link libraries as shared objects." ON)
+
+IF(NOT BUILD_SHARED_LIBS)
+    SET(VCPKG_LIBRARY_LINKAGE "static")
+ENDIF(NOT BUILD_SHARED_LIBS)
+
 # Define overlay ports (separated by `;`).
 SET(VCPKG_OVERLAY_PORTS "${CMAKE_SOURCE_DIR}/modules/overlay-ports/winpixeventruntime/;${CMAKE_SOURCE_DIR}/modules/overlay-ports/dx-agility-sdk/;${CMAKE_SOURCE_DIR}/modules/overlay-ports/d3d12-memory-allocator/;${CMAKE_SOURCE_DIR}/modules/overlay-ports/spdlog/")
