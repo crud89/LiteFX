259c8618468c9521c25aaeb4bace60e524760cc8
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2024-12-10 11:33:55 +0100

    Document vertex type.

--- src/Graphics/include/litefx/graphics.hpp
+++ src/Graphics/include/litefx/graphics.hpp
@@ -8,11 +8,29 @@ namespace LiteFX::Graphics {
     using namespace LiteFX;
     using namespace LiteFX::Math;
 
-    struct LITEFX_GRAPHICS_API Vertex {
+    /// <summary>
+    /// Default definition for a simple vertex.
+    /// </summary>
+    struct Vertex {
     public:
+        /// <summary>
+        /// The position of the vertex.
+        /// </summary>
         Vector3f Position;
+
+        /// <summary>
+        /// The color of the vertex.
+        /// </summary>
         Vector4f Color;
+
+        /// <summary>
+        /// The normal vector of the vertex.
+        /// </summary>
         Vector3f Normal;
+
+        /// <summary>
+        /// The texture coordinate of the vertex.
+        /// </summary>
         Vector2f TextureCoordinate0;
     };
 
