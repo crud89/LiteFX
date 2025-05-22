4c85e21bf22a565b939cffacc87209d28092b050
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2025-05-22 11:28:02 +0200

    Track parameters buffer beyond invocation scope.

--- src/Graphics/src/blitter_d3d12.cpp
+++ src/Graphics/src/blitter_d3d12.cpp
@@ -108,6 +108,7 @@ void Blitter<DirectX12Backend>::generateMipMaps(IDirectX12Image& image, DirectX1
 	const auto& parametersLayout = resourceBindingsLayout.descriptor(0);
 	auto parameters = device->factory().createBuffer(parametersLayout.type(), ResourceHeap::Dynamic, parametersLayout.elementSize(), image.levels());
 	parameters->map(parametersBlock, sizeof(Parameters));
+	commandBuffer.track(parameters);
 
 	// Create and bind the sampler.
 	const auto& samplerBindingsLayout = pipeline.layout()->descriptorSet(1);
