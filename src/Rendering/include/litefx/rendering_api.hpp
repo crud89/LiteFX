500d85778cffc63660d5ad8e2cd197ac8e7e213a
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2026-04-22 18:35:13 +0200

    Add adjacency and patch list primititve topologies.

--- src/Rendering/include/litefx/rendering_api.hpp
+++ src/Rendering/include/litefx/rendering_api.hpp
@@ -947,22 +947,51 @@ namespace LiteFX::Rendering {
         LineList = 0x00020001,
 
         /// <summary>
+        /// A list of lines where each vertex pair refers to the start and end points of a line and where vertices also store adjacency data.
+        /// </summary>
+        /// <seealso cref="LineList" />
+        LineListWithAdjacency = 0x00020011,
+
+        /// <summary>
         /// A list of triangles, where each triplet of vertices refers to a whole triangle.
         /// </summary>
         /// <seealso cref="TriangleStrip" />
         TriangleList = 0x00040001,
 
         /// <summary>
+        /// A list of triangles, where each triplet of vertices refers to a whole triangle and where vertices also store adjacency data.
+        /// </summary>
+        /// <seealso cref="TriangleList" />
+        TriangleListWithAdjacency = 0x00040011,
+
+        /// <summary>
         /// A strip of lines where each vertex (except the first one) refers to the end point for the next line segment.
         /// </summary>
         /// <seealso cref="LineList" />
         LineStrip = 0x00020002,
+
+        /// <summary>
+        /// A strip of lines where each vertex (except the first one) refers to the end point for the next line segment.
+        /// </summary>
+        /// <seealso cref="LineStrip" />
+        LineStripWithAdjacency = 0x00020012,
         
         /// <summary>
-        /// A strip of triangles, where each vertex (except the first two) refers to the third vertex of the next triangle segment.
+        /// A strip of triangles, where each vertex (except the first two) refers to the third vertex of the next triangle segment and where vertices also store adjacency data..
         /// </summary>
         /// <seealso cref="TriangleList" />
-        TriangleStrip = 0x00040002
+        TriangleStrip = 0x00040002,
+
+        /// <summary>
+        /// A strip of triangles, where each vertex (except the first two) refers to the third vertex of the next triangle segment and where vertices also store adjacency data..
+        /// </summary>
+        /// <seealso cref="TriangleStrip" />
+        TriangleStripWithAdjacency = 0x00040012,
+
+        /// <summary>
+        /// A list of patches used in combination with tessellation.
+        /// </summary>
+        PatchList = 0x00080001
     };
 
     /// <summary>
